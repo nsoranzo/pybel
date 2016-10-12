2e45bc4eab14f5141d237874ce8e099d7a20a24a
lekono <andrej.konotopez@scai.fraunhofer.de>
2016-10-12 17:04:41 +0200

    Added Mapping_Dict creation | Added 'on-the-fly' mapping directly from database

--- src/pybel/mapper/defaults.py
+++ src/pybel/mapper/defaults.py
@@ -5,8 +5,8 @@ and URLs to load into a new PyBEL namespace store.
 See: https://wiki.openbel.org/display/BELNA/Namespaces+Overview
 """
 
-default_namespaces = {'uniprot_human':'ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping.dat.gz',}
+default_namespaces = {'uniprot_human_map':'ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping.dat.gz'}
 
-# TODO: add HGNC and other NSs
+# TODO: add HGNC and other NSs for ID decoding!
 
 #                       'HGNC':'ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/json/hgnc_complete_set.json'}
--- src/pybel/mapper/mapper.py
+++ src/pybel/mapper/mapper.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
+# TODO: Change print('[INFO] ...') to logging!
+
 class Mapper:
     
     def __init__(self, connection_string, sql_echo=False):
@@ -15,7 +17,6 @@ class Mapper:
         """
         self.db_engine = create_engine(connection_string,echo=sql_echo)
         self.db_session = scoped_session(sessionmaker(bind=self.db_engine,autoflush=False,expire_on_commit=False))
-        self.map_dict = {}
     
     def create_tables(self):
         """
@@ -61,61 +62,112 @@ class Mapper:
                 print("[INFO] Done bulk_insert ({runtime:3.2f}s)".format(runtime=(time.time()-tmp_time)))
                 tmp_time = time.time()
                 
-                self.map_dict['uniprot'] = uniprot_dict
-                
             elif source_url.endswith('.json'):
                 pass
             
             print("[INFO] Done parsing '{ns}'. \n[RUNTIME] {runtime:3.2f}s".format(ns=namespace,runtime=(time.time()-start_time)))
-            
-    def __create_mapping(self, reference, reference_id, uniprot_id):        
-            map = {}
-            pass
+                
         
-    def create_namespace_map(self, namespace):
+    def create_uniprot_namespace_map(self, namespace):
         """
-        Returns dictionary for mapping between UniProt and namespace.
+        :param: namespace: Identifier for namespaces that should be mapped to. Use get_namespace_list() to get a list of available namespaces.
+        
+        Creates a dictionary of sets that contains mapping for uniprot identifiers. i.e. {uniprot_id:{namespace_id, set, ...}}
         """
         start_time = time.time()
-        mapping_data = self.db_session.query(database_models.Uniprot_Map).filter_by(reference=namespace).all()
+        uniprot_orm = database_models.Uniprot_Map
+        mapping_data = self.db_session.query(uniprot_orm).filter_by(reference=namespace).all()
         
         uniprot_dict = {}
         namespace_dict = {}
         
         for entry in mapping_data:
             uniprot_id = entry.uniprot
-            reference = entry.reference
+            ref = entry.reference
             reference_id = entry.xref
-            
-            if uniprot_id in uniprot_dict and reference in uniprot_dict[uniprot_id]:
-                if reference_id not in uniprot_dict[uniprot_id][reference]:
-                    uniprot_dict[uniprot_id][reference].append(reference_id)
-             
-            elif uniprot_id in uniprot_dict:
-                uniprot_dict[uniprot_id][reference] = [reference_id]
+              
+            if uniprot_id in uniprot_dict:
+                uniprot_dict[uniprot_id].add(reference_id)
              
             else:
-                uniprot_dict[uniprot_id] = {reference:[reference_id]}
+                uniprot_dict[uniprot_id] = set([reference_id])
             
-        #TODO: Add Namespace -> Uniprot
+            if reference_id in namespace_dict:
+                namespace_dict[reference_id].add(uniprot_id)
+                 
+            else:
+                namespace_dict[reference_id] = set([uniprot_id])
+                
+        result_dict = {'UniProt':uniprot_dict,
+                       namespace:namespace_dict}
+        
+        print('[INFO] Map for "{mapped_namespace}" created ({runtime:3.2f}s)'.format(mapped_namespace=namespace,runtime=(time.time()-start_time)))
+        
+        return result_dict
+    
+    def create_map(self):
+        """
+        Creates mapping dictionary. i.e. {namespace_A: {namespace_A:{Identifier:{uniprot, id, set, ...}}, 'UniProt':{Identifier:{namespace, id, set, ...}, ...}}, ...}
+        To access uniprot identifiers in namespace 'HGNC' for 'HGNC:620' (APP) use: map_dict['HGNC']['HGNC']['HGNC:620']
+        """
+        start_time = time.time()
+        tmp_map_dict = {}
+        map_dict = {}
+        uniprot_dict = {}
+        
+        for namespace in self.get_namespace_list():
+            tmp_map_dict = self.create_uniprot_namespace_map(namespace)
             
-        mapping_dict = {'UniProt':uniprot_dict,
-                        namespace:namespace_dict}
-        
-        print('[INFO] Mapping_Data type: {mapping_data_type} ({runtime:3.2f}s)'.format(mapping_data_type=type(mapping_data),runtime=(time.time()-start_time)))
-        
-        return mapping_dict
+            for uni_id in tmp_map_dict['UniProt']:
+                if uni_id in uniprot_dict and namespace in uniprot_dict[uni_id]:
+                    uniprot_dict[uni_id][namespace] |= tmp_map_dict['UniProt'][uni_id]
+                elif uni_id in uniprot_dict:
+                    uniprot_dict[uni_id][namespace] = tmp_map_dict['UniProt'][uni_id]
+                else:
+                    uniprot_dict[uni_id] = {namespace:tmp_map_dict['UniProt'][uni_id]}
 
-    def map(self, source_namespace, name, target_namesapce, use_uniprot_api=False):
+            del(tmp_map_dict['UniProt'])
+            map_dict.update(tmp_map_dict)
+            
+        map_dict['UniProt'] = uniprot_dict          
+        
+        print('[INFO] Created mapping dictionary ({runtime:3.2f}s)'.format(runtime=(time.time()-start_time)))
+        
+        return map_dict
+    
+    def get_namespace_list(self):
+        """
+        Returns a list of all mapped to namespaces.
+        
+        :return: List of namespaces available in database.
+        """
+        namespaces_nested = self.db_session.query(database_models.Uniprot_Map.reference).distinct().all()
+        return [namespace for namespaces_listed in namespaces_nested for namespace in namespaces_listed]
+    
+    def map(self, source_namespace, identifier, target_namespace, use_uniprot_api=False):
         """
         :param: source_namespace: Namespace name is valid in.
         :param: name: Name that should be mapped to target_namespace.
         :param: target_namespace: Namespace name should be translated to.
-        :param: use_uniprot_api: Describes if online api of uniprot or local database should be used for mapping.
+        :param: use_uniprot_api: Describes if online api of uniprot or local database should be used for mapping. [This is a placeholder and not used yet!]
         
         :return: Name in target_namespace.
         
         Get map for name in source_namespace to target_namespace.
         """
-        pass
-    
+        start_time = time.time()
+        uniprot_orm = database_models.Uniprot_Map
+        
+        uniprot_nested = self.db_session.query(uniprot_orm.uniprot).filter_by(reference=source_namespace,xref=identifier).all()
+        
+        uniprot_ids = [uniprot_id for listed in uniprot_nested for uniprot_id in listed]
+        
+        
+        desired_targets = self.db_session.query(uniprot_orm.xref).filter_by(reference=target_namespace).filter(uniprot_orm.uniprot.in_(uniprot_ids)).all()
+
+        print("[INFO] Mapped '{entity_name}' from '{source_name}' to '{target_name}' ({runtime:3.2f}s)".format(entity_name=identifier,
+                                                                                                               source_name=source_namespace,
+                                                                                                               target_name=target_namespace,
+                                                                                                               runtime=(time.time()-start_time)))
+        
+        return [target_id for listed in desired_targets for target_id in listed]
