f426a218559853306399debe3ae816c218bbe2f0
lekono <andrej.konotopez@scai.fraunhofer.de>
2016-11-16 13:45:29 +0100

    Updated database_models
    Added relational store tables for BEL (have to finish modification table!)
    [Work in progress]

--- src/pybel/manager/database_models.py
+++ src/pybel/manager/database_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Table, Binary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -6,6 +6,13 @@ DEFINITION_TABLE_NAME = 'pybel_cache_definition'
 CONTEXT_TABLE_NAME = 'pybel_cache_context'
 DEFINITION_NAMESPACE = 'N'
 DEFINITION_ANNOTATION = 'A'
+
+FUNCTION_TABLE_NAME = 'pybel_function'
+TERM_TABLE_NAME = 'pybel_term'
+STATEMENT_TABLE_NAME = 'pybel_statement'
+MODIFICATION_TABLE_NAME = 'pybel_modification'
+EDGEPROPERTY_TABLE_NAME = 'pybel_property'
+GRAPH_TABLE_NAME = 'pyel_graph_store'
 
 Base = declarative_base()
 
@@ -32,6 +39,66 @@ class Context(Base):
     __tablename__ = CONTEXT_TABLE_NAME
 
     id = Column(Integer, primary_key=True)
-    definition_id = Column(Integer, ForeignKey('pybel_cache_definition.id'), index=True)
+    definition_id = Column(Integer, ForeignKey('{}.id'.format(DEFINITION_TABLE_NAME)), index=True)
     context = Column(String(255))
     encoding = Column(String(50))
+
+
+class BELFunction(Base):
+    __tablename__ = FUNCTION_TABLE_NAME
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True)
+
+
+class BELModification(Base):
+    __tablename__ = MODIFICATION_TABLE_NAME
+
+    id = Column(Integer, primary_key=True)
+
+
+association_table = Table('pybel_statement_properties', Base.metadata,
+                          Column('statement_id', Integer, ForeignKey('{}.id'.format(STATEMENT_TABLE_NAME))),
+                          Column('property_id', Integer, ForeignKey('{}.id'.format(EDGEPROPERTY_TABLE_NAME)))
+                          )
+
+
+class BELTerm(Base):
+    __tablename__ = TERM_TABLE_NAME
+
+    id = Column(Integer, primary_key=True)
+    function_id = Column(Integer, ForeignKey('{}.id'.format(FUNCTION_TABLE_NAME)))
+    nsContext_id = Column(Integer, ForeignKey('{}.id'.format(CONTEXT_TABLE_NAME)))
+    modification_id = Column(Integer, ForeignKey('{}.id'.format(MODIFICATION_TABLE_NAME)))
+
+
+class BELStatement(Base):
+    __tablename__ = STATEMENT_TABLE_NAME
+
+    id = Column(Integer, primary_key=True)
+    subject_id = Column(Integer, ForeignKey('{}.id'.format(TERM_TABLE_NAME)))
+    relation = Column(String(50))
+    object_id = Column(Integer, ForeignKey('{}.id'.format(TERM_TABLE_NAME)))
+    subject = relationship("BELTerm", foreign_keys=[subject_id])
+    object = relationship("BELTerm", foreign_keys=[object_id])
+    properties = relationship("BELEdgeProperty",
+                              secondary=association_table,
+                              backref="statements")
+
+
+class BELEdgeProperty(Base):
+    __tablename__ = EDGEPROPERTY_TABLE_NAME
+
+    id = Column(Integer, primary_key=True)
+    propKey = Column(String(255))
+    relativeKey = Column(String(255))
+    propValue = Column(String(255))
+
+
+class PyBELGraphStore(Base):
+    __tablename__ = GRAPH_TABLE_NAME
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=True)
+    label = Column(String(25))
+    graph = Column(Binary)
